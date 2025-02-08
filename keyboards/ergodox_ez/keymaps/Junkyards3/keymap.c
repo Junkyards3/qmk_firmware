b9f653f9a4f56ba559877d05b2c95da23eccde0a
hfavereau <thehwanderer@gmail.com>
2025-02-08 23:38:18 +0100

    some changes

--- keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
+++ keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
@@ -28,21 +28,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT_ergodox_pretty(
        KC_CAPS,    KC_1,    KC_2,         KC_3,           KC_4,            KC_5,       KC_MINS,      KC_EQL        , KC_6           , KC_7   , KC_8   , KC_9  , KC_0   , KC_LBRC,
-        KC_TAB,    KC_Q,    KC_W,         KC_E,           KC_R,            KC_T,         TG(2),      KC_BSPC       , KC_Y           , KC_U   , KC_I   , KC_O  , KC_P   , KC_RBRC,
+        KC_TAB,    KC_Q,    KC_W,         KC_E,           KC_R,            KC_T,         TG(2),      KC_ENT        , KC_Y           , KC_U   , KC_I   , KC_O  , KC_P   , KC_RBRC,
 LCTL_T(KC_ESC),    KC_A,    KC_S,         KC_D,           KC_F,            KC_G,                                     KC_H           , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
        KC_LSFT,    KC_Z,    KC_X,         KC_C,           KC_V,            KC_B,         TG(1),      KC_LT         , KC_N           , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_NUHS,
- LSFT(KC_LCTL), KC_LGUI, KC_LALT, LSFT(KC_TAB),         KC_TAB,                                                                       KC_LEFT, KC_DOWN, KC_UP , KC_RGHT, KC_RCTL,
+ LSFT(KC_LCTL), KC_LGUI, KC_LALT, LSFT(KC_TAB),         KC_TAB,                                                                       KC_ENT , KC_LALT, KC_UP , KC_RGHT, KC_RALT,
 
-                                                                         KC_DEL,         TG(3),      ALL_T(MS_BTN2), MEH_T(MS_BTN1) ,
+                                                                         KC_DEL,         TG(3),      OSM(MOD_HYPR), OSM(MOD_MEH) ,
                                                                                        KC_PGDN,      KC_PGUP       ,
-                                                LSFT_T(KC_SPC), LCTL_T(KC_LEFT), LT(1,KC_DOWN),      LT(2,KC_UP)   , LSFT_T(KC_RGHT), KC_ENT
+                                                LSFT_T(KC_SPC),  LT(1,KC_LEFT), LCTL_T(KC_DOWN),     LSFT_T(KC_UP), LT(2,KC_RGHT)   , LCTL_T(KC_BSPC)
 ),
 
 [1] = LAYOUT_ergodox_pretty(
 KC_TRNS,      KC_F1,      KC_F2,      KC_F3,        KC_F4,        KC_F5,   KC_F6,      KC_F7  , KC_F8     , KC_F9     , KC_F10       , KC_F11 , KC_F12    , KC_TRNS,
 KC_TRNS,       KC_8, RALT(KC_0), RALT(KC_4), RALT(KC_EQL),      KC_PPLS, KC_TRNS,      KC_TRNS, RALT(KC_8), RALT(KC_7), LSFT(KC_NUBS), KC_NUBS, RALT(KC_6), KC_TRNS,
 KC_TRNS, RALT(KC_3),    KC_RBRC,       KC_5,      KC_MINS,      KC_PAST,                        KC_LEFT   , KC_DOWN   , KC_UP        , KC_RGHT, KC_1      , KC_TRNS,
-KC_TRNS, RALT(KC_2),    KC_LBRC, RALT(KC_5),   RALT(KC_5), LSFT(KC_DOT), KC_TRNS,      KC_TRNS, KC_QUOT   , KC_TRNS   , KC_TRNS      , KC_TRNS, KC_TRNS   , KC_TRNS,
+KC_TRNS, RALT(KC_2),    KC_LBRC, RALT(KC_5),   RALT(KC_MINS), LSFT(KC_DOT), KC_TRNS,      KC_TRNS, KC_QUOT   , KC_TRNS   , KC_TRNS      , KC_TRNS, KC_TRNS   , KC_TRNS,
 KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,                                                  KC_TRNS   , KC_TRNS      , KC_TRNS, KC_TRNS   , KC_TRNS,
 
                                                                 KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS   ,
