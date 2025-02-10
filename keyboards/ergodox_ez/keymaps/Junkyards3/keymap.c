61c1944d49e776919fe1b7035b01ab6179872a98
hfavereau <thehwanderer@gmail.com>
2025-02-10 23:13:15 +0100

    minor adjustments

--- keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
+++ keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
@@ -31,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,    KC_W,         KC_E,           KC_R,            KC_T,         TG(2),      KC_ENT        , KC_Y           , KC_U   , KC_I   , KC_O  , KC_P   , KC_RBRC,
 LCTL_T(KC_ESC),    KC_A,    KC_S,         KC_D,           KC_F,            KC_G,                                     KC_H           , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
        KC_LSFT,    KC_Z,    KC_X,         KC_C,           KC_V,            KC_B,         TG(1),      KC_LT         , KC_N           , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_NUHS,
- LSFT(KC_LCTL), KC_LGUI, KC_LALT, LSFT(KC_TAB),         KC_TAB,                                                                       KC_ENT , KC_LALT, KC_UP , KC_RGHT, KC_RALT,
+ LSFT(KC_LCTL), KC_LGUI, KC_LALT, LSFT(KC_TAB),         KC_TAB,                                                                       KC_ENT , KC_LALT, KC_RALT, KC_RGUI, LSFT(KC_LCTL),
 
                                                                          KC_DEL,         TG(3),      OSM(MOD_HYPR), OSM(MOD_MEH) ,
                                                                                        KC_PGDN,      KC_PGUP       ,
-                                                LSFT_T(KC_SPC),  LT(1,KC_LEFT), LCTL_T(KC_DOWN),     LSFT_T(KC_UP), LT(2,KC_RGHT)   , LCTL_T(KC_BSPC)
+                                                LSFT_T(KC_SPC),  LT(2,KC_LEFT), LCTL_T(KC_DOWN),     LSFT_T(KC_UP), LT(1,KC_RGHT)   , LCTL_T(KC_BSPC)
 ),
 
 [1] = LAYOUT_ergodox_pretty(
