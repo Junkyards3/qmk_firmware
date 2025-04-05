fdbd89d7e42301da8fcfba49d90ed0beede8809a
hfavereau <thehwanderer@gmail.com>
2025-04-05 16:06:17 +0200

    some adjustments

--- keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
+++ keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT_ergodox_pretty(
        KC_CAPS,    KC_1,    KC_2,         KC_3,           KC_4,          KC_5,  OSM(MOD_LCTL),      KC_EQL        , KC_6        , KC_7           , KC_8   , KC_9   , KC_0   , KC_LBRC,
-        KC_TAB,    KC_Q,    KC_W,         KC_E,           KC_R,          KC_T,         KC_EQL,      KC_ENT        , KC_Y        , KC_U           , KC_I   , KC_O   , KC_P   , KC_RBRC,
+        TG(3) ,    KC_Q,    KC_W,         KC_E,           KC_R,          KC_T,         KC_EQL,      KC_ENT        , KC_Y        , KC_U           , KC_I   , KC_O   , KC_P   , KC_RBRC,
 LCTL_T(KC_ESC),    KC_A,    KC_S,         KC_D,           KC_F,          KC_G,                                      KC_H        , KC_J           , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
        KC_LSFT,    KC_Z,    KC_X,         KC_C,           KC_V,          KC_B,     RALT(KC_8),      KC_LT         , KC_N        , KC_M           , KC_COMM, KC_DOT , KC_SLSH, KC_NUHS,
  LSFT(KC_LCTL), KC_LGUI, KC_LALT, LSFT(KC_TAB),         KC_TAB,                                                                   KC_ENT         , KC_LALT, KC_RALT, KC_RGUI, OSM(MOD_LCTL|MOD_LALT)  ,
 
                                                                        KC_DEL,          TG(3),      OSM(MOD_HYPR) , OSM(MOD_MEH),
                                                                                       KC_PGDN,      KC_PGUP       ,
-                                                LSFT_T(KC_SPC), LT(2,KC_PSLS), LCTL_T(KC_F13),      LSFT_T(KC_F14), LT(1,KC_DOT), LCTL_T(KC_BSPC)
+                                                LSFT_T(KC_SPC), LT(2,KC_EQL), LCTL_T(KC_F13),      LSFT_T(KC_F14), LT(1,KC_PDOT), LCTL_T(KC_BSPC)
 ),
 
 [1] = LAYOUT_ergodox_pretty(
@@ -64,7 +64,7 @@ KC_TRNS, KC_EXLM, KC_AT, KC_HASH,     KC_DLR, KC_PERC,   KC_NO,      KC_NO  , KC
   KC_NO,   KC_NO, KC_NO,    KC_1,       KC_4,    KC_6, KC_TRNS,      KC_TRNS, KC_NO  , KC_AMPR, KC_ASTR, KC_LPRN, KC_NO  , KC_NO,
   KC_NO,   KC_NO, KC_NO,  KC_SPC,       KC_3,    KC_8,                        KC_NO  , KC_DLR , KC_PERC, KC_CIRC, KC_NO  , KC_NO,
   KC_NO,   KC_NO, KC_NO, KC_BSPC, RALT(KC_7),  KC_EQL, KC_TRNS,      KC_TRNS, KC_NO  , KC_EXLM, KC_AT  , KC_HASH, KC_NO  , KC_NO,
-  KC_NO,   KC_NO, KC_NO,   KC_NO,      KC_NO,                                          KC_RPRN, KC_RPRN, KC_LT  , KC_NO  , KC_NO,
+  KC_NO,   KC_NO, KC_NO,   KC_NO,      KC_NO,                                          KC_NO  , KC_RPRN, KC_LT  , KC_NO  , KC_NO,
 
                                               KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS,
                                                        KC_TRNS,      KC_TRNS,
