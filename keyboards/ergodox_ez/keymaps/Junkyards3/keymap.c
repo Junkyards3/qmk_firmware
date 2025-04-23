b3d5cd236d525061874c33e82a61f578923ba216
hfavereau <thehwanderer@gmail.com>
2025-04-23 13:30:23 +0200

    switch ctrl and shift on all layers, add copy/paste and undo to thumb left

--- keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
+++ keyboards/ergodox_ez/keymaps/Junkyards3/keymap.c
@@ -40,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TG(6),         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T, RALT(KC_0),      KC_ENT , KC_Y         , KC_U         , KC_I        , KC_O        , KC_P           , KC_LBRC,
  KC_ESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),         KC_G,                           KC_H         , RSFT_T(KC_J) , RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
 KC_LSFT,         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, RALT(KC_8),      KC_LT  , KC_N         , KC_M         , KC_COMM     , KC_DOT      , KC_SLSH        , CW_TOGG ,
-KC_LCTL,      KC_LGUI,      KC_LALT, LSFT(KC_TAB),LSFT_T(KC_TAB),                                                       KC_ENT       , LSFT(KC_ENT), KC_RALT     , QK_AREP          , QK_REP  ,
+KC_LCTL,      KC_LGUI,      KC_LALT, LSFT(KC_TAB),LSFT_T(KC_TAB),                                                       KC_ENT       , LSFT(KC_ENT), KC_RALT     , QK_AREP        , QK_REP  ,
 
-                                                                        DT_PRNT,      DT_UP,      KC_NO  , KC_NO        ,
-                                                                                    DT_DOWN,      KC_NO  ,
-                                                   LT(1,KC_SPC), LT(2,KC_EQL),    CW_TOGG,      KC_CAPS, LT(4,KC_PSLS), LT(3,KC_BSPC)
+                                                                        KC_COPY,   KC_PSTE,     DT_UP  , DT_PRNT        ,
+                                                                                   KC_UNDO,     DT_DOWN  ,
+                                                   LT(1,KC_SPC), LT(2,KC_EQL),     CW_TOGG,     KC_CAPS, LT(4,KC_PSLS), LT(3,KC_BSPC)
 ),
 
 [1] = LAYOUT_ergodox_pretty(
 KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO ,
 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,      KC_TRNS, KC_INS , KC_HOME, KC_COPY, KC_PSTE, KC_CUT , KC_NO ,
-KC_NO, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,   KC_NO,                        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_UNDO, KC_NO ,
+KC_NO, KC_LGUI, KC_LALT,   KC_LCTL, KC_LSFT,   KC_NO,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_UNDO, KC_NO ,
 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,      KC_TRNS, KC_DEL , KC_PGDN, KC_PGUP, KC_NO  , KC_NO  , QK_REP,
 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
 
@@ -62,7 +62,7 @@ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                      
 [2] = LAYOUT_ergodox_pretty(
 KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,   KC_NO,      KC_NO  , KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO ,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,      KC_TRNS, KC_PPLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_PMNS, KC_NO ,
-  KC_NO, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,   KC_NO,                        KC_PEQL, KC_DLR , KC_PERC, KC_CIRC, KC_PAST, KC_NO ,
+  KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,   KC_NO,                        KC_PEQL, KC_DLR , KC_PERC, KC_CIRC, KC_PAST, KC_NO ,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,      KC_TRNS, KC_RPRN, KC_EXLM, KC_AT  , KC_HASH, KC_PSLS, QK_REP,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_NO  , KC_RPRN, KC_NO  , KC_NO  , KC_NO ,
 
@@ -74,7 +74,7 @@ KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,   KC_NO,      KC_NO  , KC_
 [3] = LAYOUT_ergodox_pretty(
 KC_NO,      KC_NO,      KC_NO,         KC_NO,      KC_NO,      KC_NO,   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
 KC_NO,       KC_1, RALT(KC_0),       KC_BSLS,       KC_4,       KC_6,   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
-KC_NO, RALT(KC_3),    KC_RBRC,    RALT(KC_8),       KC_3,       KC_8,                        KC_NO  , KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO ,
+KC_NO, RALT(KC_3),    KC_RBRC,    RALT(KC_8),       KC_3,       KC_8,                        KC_NO  , KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO ,
 KC_NO, RALT(KC_2), RALT(KC_9), LSFT(KC_QUOT), RALT(KC_7), RALT(KC_6),   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , QK_REP,
 KC_NO,      KC_NO,      KC_NO,         KC_NO,      KC_NO,                                             KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
 
@@ -86,7 +86,7 @@ KC_NO,      KC_NO,      KC_NO,         KC_NO,      KC_NO,                       
 [4] = LAYOUT_ergodox_pretty(
 KC_NO,   KC_NO,         KC_NO,      KC_NO,         KC_NO,   KC_NO,   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
 KC_NO,   KC_NO,         KC_NO, RALT(KC_4),  RALT(KC_EQL),   KC_NO,   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
-KC_NO, KC_NUBS, LSFT(KC_NUBS),       KC_5,       KC_MINS,   KC_NO,                        KC_NO  , KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO ,
+KC_NO, KC_NUBS, LSFT(KC_NUBS),       KC_5,       KC_MINS,   KC_NO,                        KC_NO  , KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO ,
 KC_NO,   KC_NO,         KC_NO, RALT(KC_5), RALT(KC_MINS),   KC_NO,   KC_NO,      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , QK_REP,
 KC_NO,   KC_NO,         KC_NO,      KC_NO,         KC_NO,                                          KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO ,
 
